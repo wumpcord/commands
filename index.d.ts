ab6f0f21b6cda02ae3480139caa275afb64dc19a
Noel <august@augu.dev>
2021-02-01 04:38:09 -0700

    Start of typings

---
+++ index.d.ts
@@ -0,0 +1,11 @@
+/**
+ * Typings made for package `@wumpcord/commands`
+ *
+ * Project: https://github.com/wumpcord/commands
+ * Definitions By:
+ *    - August <cutie@floofy.dev>
+ */
+
+declare module '@wumpcord/commands' {
+  import * as wumpcord from 'wumpcord';
+}
